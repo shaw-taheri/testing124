2c64681b7f1fea78a33697ccc1dc525c18f2b839
Hamface <samb.humphrey@gmail.com>
2014-10-30 12:02:55 -0700

    Added global Nav to master

--- Skejooler/Articles.aspx.cs
+++ Skejooler/Articles.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Skejooler
 {
-    public partial class AboutUs : System.Web.UI.Page
+    public partial class Articles : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
