e8877ec09d86c5d3478e5a3a1102f3cbf915778f
Trevor-Wilson <twilson_777@hotmail.com>
2014-10-31 13:03:33 -0700

    i dont know

--- Skejooler/Index.aspx.designer.cs
+++ Skejooler/Index.aspx.designer.cs
@@ -20,59 +20,5 @@ namespace Skejooler {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// homeCenterImg control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image homeCenterImg;
-        
-        /// <summary>
-        /// easySearchHomeImg control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image easySearchHomeImg;
-        
-        /// <summary>
-        /// scheduleHomeImg control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image scheduleHomeImg;
-        
-        /// <summary>
-        /// invigHomeImg control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image invigHomeImg;
-        
-        /// <summary>
-        /// jobPostImgHome control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image jobPostImgHome;
-        
-        /// <summary>
-        /// onlineProgramsHomeImg control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image onlineProgramsHomeImg;
     }
 }
