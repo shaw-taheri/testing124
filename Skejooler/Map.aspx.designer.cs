aea7a32fdfb1101942d228f14f00706725207c47
mattybeaulne <mattybeaulne@gmail.com>
2014-11-13 10:27:48 -0800

    mapstuff

--- Skejooler/Map.aspx.designer.cs
+++ Skejooler/Map.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace Skejooler {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource CentreList;
+        
+        /// <summary>
+        /// RegButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button RegButton;
     }
 }
