3cfdaa13f6d45c26916823f4e41c056f3911fad4
Trevor-Wilson <twilson_777@hotmail.com>
2014-11-13 14:18:48 -0800

    DetailsView in AdminProfile page working

--- Skejooler/AdminProfile.aspx.designer.cs
+++ Skejooler/AdminProfile.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace Skejooler {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// DetailsView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
+        
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
     }
 }
