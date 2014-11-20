45d0cae11237953695cf2dd49ddf9deb3ea8cf4c
mattybeaulne <mattybeaulne@gmail.com>
2014-11-20 12:32:03 -0800

    deleted city filter, added map zoom and center on city choice

--- Skejooler/Map.aspx.cs
+++ Skejooler/Map.aspx.cs
@@ -13,15 +13,12 @@ namespace Skejooler
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string markers = GetMarkers();
-
             if (!IsPostBack)
             {
-                Label1.Text = "100 Mile House";
-            }
+                string markers = GetMarkers();
 
-            //instatiate google map
-            Literal1.Text = @"
+                //instatiate google map
+                Literal1.Text = @"
              <script type='text/javascript'>
      
                 function initialize() { 
@@ -38,16 +35,16 @@ namespace Skejooler
                     var myMap = new google.maps.Map(document.getElementById('mapArea'),
                      mapOptions);"
 
-                     + markers +
+                         + markers +
 
-                @"}     
+                    @"}     
                 google.maps.event.addDomListener(window, 'load', initialize);
 
              </script>";
-            
-            
-        }
 
+
+            }
+        }
         // create markers for the map based on lat and long in database
         protected string GetMarkers()
         {
@@ -56,7 +53,7 @@ namespace Skejooler
             using (MySqlConnection con = new MySqlConnection("Server=localhost;Database=skejooler;UID=root;Password="))
             {
                 //select lat and long from database
-                MySqlCommand cmd = new MySqlCommand("Select latitude, longitude, centre_id, name FROM invigilation_centre WHERE city ='" + Label1.Text.ToString() + "'", con);
+                MySqlCommand cmd = new MySqlCommand("Select latitude, longitude, centre_id, name FROM invigilation_centre ", con);
                 con.Open();
                 MySqlDataReader reader = cmd.ExecuteReader();
                 int i = 0;
@@ -77,16 +74,120 @@ namespace Skejooler
             return markers;
         }
 
+        protected string GetCityMarkers()
+        {
+            string cityMarkers = "";
+            // connect to database
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;Database=skejooler;UID=root;Password="))
+            {
+                //select lat and long from database
+                MySqlCommand cmd = new MySqlCommand("Select latitude, longitude, centre_id, name FROM invigilation_centre WHERE city ='"+ DropDownList1.SelectedItem.ToString() +"'", con);
+                con.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                int i = 0;
+
+                //create markers
+                while (reader.Read())
+                {
+                    i++;
+                    cityMarkers +=
+                    @"var marker" + i.ToString() + @" = new google.maps.Marker({
+              position: new google.maps.LatLng(" + reader["Latitude"].ToString() + ", " +
+                    reader["Longitude"].ToString() + ")," +
+                    @"map: myMap,
+              title:'" + reader["name"].ToString() + "'});";
+                }
+            }
+
+            return cityMarkers;
+        }
+
+        protected string GetLatitude()
+        {
+            string latitude = "";
+            // connect to database
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;Database=skejooler;UID=root;Password="))
+            {
+                //select lat and long from database
+                MySqlCommand cmd = new MySqlCommand("Select latitude, longitude, centre_id, name FROM invigilation_centre WHERE city ='" + DropDownList1.SelectedItem.ToString() + "'", con);
+                con.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                int i = 0;
+
+                //create markers
+                while (reader.Read())
+                {
+                    latitude = reader["Latitude"].ToString();
+                }
+
+                
+            }
+            return latitude;
+        }
+
+        protected string GetLongitude()
+        {
+            string longitude = "";
+            // connect to database
+            using (MySqlConnection con = new MySqlConnection("Server=localhost;Database=skejooler;UID=root;Password="))
+            {
+                //select lat and long from database
+                MySqlCommand cmd = new MySqlCommand("Select latitude, longitude, centre_id, name FROM invigilation_centre WHERE city ='" + DropDownList1.SelectedItem.ToString() + "'", con);
+                con.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                int i = 0;
+
+                //create markers
+                while (reader.Read())
+                {
+                    longitude = reader["Longitude"].ToString();
+                }
+
+
+            }
+            return longitude;
+        }
+
+            
         
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
-            
+            {
+                string cityMarkers = GetCityMarkers();
+                 //instatiate google map
+                Literal1.Text = @"
+             <script type='text/javascript'>
+     
+                function initialize() { 
+    
+                    var lat = " + GetLatitude().ToString() + @"
+                    var long = "  + GetLongitude().ToString() + @"
+   
+                    var mapOptions = {
+                         center: new google.maps.LatLng(lat, long),
+                         zoom: 11,
+                         mapTypeId: google.maps.MapTypeId.ROADMAP
+                     };
+                    var infoWindow = new google.maps.InfoWindow();
+                    var myMap = new google.maps.Map(document.getElementById('mapArea'),
+                     mapOptions);"
+
+                         + cityMarkers +
+
+                    @"}     
+                google.maps.event.addDomListener(window, 'load', initialize);
+
+             </script>"; ;
+
+
+            }
             //when city is changed in drop down menu, redraw markers
             CentreList.SelectCommand = "SELECT name, street_name, city, province, postal_code, phone_num, cost FROM invigilation_centre WHERE city ='" + DropDownList1.SelectedItem.ToString() + "'";
-            DropDownList2.SelectedValue = DropDownList1.SelectedValue;
-            Label1.Text = DropDownList1.SelectedItem.ToString();
+            
+            Label1.Text = this.DropDownList1.SelectedItem.ToString();
+            
             
         }
 
@@ -95,26 +196,8 @@ namespace Skejooler
             Response.Redirect("RegForm.aspx");
         }
 
-        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-            CentreList.SelectCommand = "SELECT name, street_name, city, province, postal_code, phone_num, cost FROM invigilation_centre WHERE name ='" + DropDownList2.SelectedItem.ToString() + "'";
-            DropDownList1.SelectedValue = DropDownList2.SelectedValue;
-            
-        }
-
-        protected void Button1_Click1(object sender, EventArgs e)
-        {
-            string location;
-            Session["userLocation"] = DropDownList2.SelectedItem;
-            location = Session["userLocation"].ToString();
-        }
-
-        protected void Label1_PreRender(object sender, EventArgs e)
-        {
-
-        }
-
+       
+        
         
     }
 }
--- Skejooler/Map.aspx.designer.cs
+++ Skejooler/Map.aspx.designer.cs
@@ -22,15 +22,6 @@ namespace Skejooler {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
-        /// DropDownList2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
-        
-        /// <summary>
         /// namedropdown control.
         /// </summary>
         /// <remarks>
